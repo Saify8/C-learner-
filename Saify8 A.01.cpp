ef3b61f0b879b44fe1ae82307c5f28d7d16a1633
Saif Shikrani <124202859+Saify8@users.noreply.github.com>
2023-02-02 05:17:06 +0500

    Add files via upload
    Name intro 🧐

---
+++ Saify8 A.01.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name ;
+    name="Saif Siddique \n";
+    string id;
+    id="bsf2205767\n";
+    cout<<name;
+    cout <<id;
+    int a,b;
+    a=50;
+    b=10;
+    cout<<"a+b="<<a+b;
+    cout<<"\na-b="<<a-b;
+    cout<<"\na*b="<<a*b;
+    cout<<"\na/b="<<a/b;
+    cout<<"\na%b="<<a%b;
+    return 0;
+}
